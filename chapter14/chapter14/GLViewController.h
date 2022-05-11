673c03071bbb0a59a25fca5736332426659b020e
张理想 <zhanglixiang@ececloud.cn>
2022-05-11 16:08:27 +0800

    图像IO

---
+++ chapter14/chapter14/GLViewController.h
@@ -0,0 +1,17 @@
+//
+//  GLViewController.h
+//  chapter14
+//
+//  Created by 张理想 on 2022/5/11.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+/// 加载和显示PVRTC图片
+
+@interface GLViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ chapter14/chapter14/TableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TableViewController.h
+//  chapter14
+//
+//  Created by 张理想 on 2022/5/11.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TableViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

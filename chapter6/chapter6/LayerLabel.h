84fefbfa0e7d5314cacdee63b6c3a9e0da70412a
张理想 <zhanglixiang@ececloud.cn>
2022-05-11 16:09:14 +0800

    专用图层

---
+++ chapter6/chapter6/LayerLabel.h
@@ -0,0 +1,16 @@
+//
+//  LayerLabel.h
+//  chapter6
+//
+//  Created by 张理想 on 2022/5/11.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LayerLabel : UILabel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ chapter6/chapter6/ScrollView.m
@@ -0,0 +1,61 @@
+//
+//  ScrollView.m
+//  chapter6
+//
+//  Created by 张理想 on 2022/5/11.
+//
+
+#import "ScrollView.h"
+
+@implementation ScrollView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+    [self setup];
+}
+
++ (Class)layerClass {
+    return [CAScrollLayer class];
+}
+
+- (void)setup {
+    // enable clipping
+    self.layer.masksToBounds = YES;
+    
+    // attach pan gesture recongnizer
+    UIPanGestureRecognizer *recognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(pan:)];
+    [self addGestureRecognizer:recognizer];
+    
+    UIImage *image = [UIImage imageNamed:@"cat"];
+    
+    CALayer *imageLayer = [CALayer layer];
+    [imageLayer setContentsScale:[[UIScreen mainScreen] scale]];
+    [imageLayer setContents:(__bridge id)[image CGImage]];
+    [imageLayer setBounds:CGRectMake(0.0, 0.0, [image size].width, [image size].height)];
+    [imageLayer setAnchorPoint:CGPointMake(0.0, 0.0)];
+    
+    [(CAScrollLayer *)self.layer addSublayer:imageLayer];
+}
+
+- (void)pan:(UIPanGestureRecognizer *)recognizer {
+    //get the offset by subtracting the pan gesture
+    //translation from the current bounds origin
+    CGPoint offset = self.bounds.origin;
+    offset.x -= [recognizer translationInView:self].x;
+    offset.y -= [recognizer translationInView:self].y;
+    
+    // scroll the layer
+    [(CAScrollLayer *)self.layer scrollToPoint:offset];
+    
+    // reset the pan gesture translation
+    [recognizer setTranslation:CGPointZero inView:self];
+}
+
+@end
